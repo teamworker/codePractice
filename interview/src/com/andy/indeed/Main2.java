a0eaffaed346c7ff786d5cf04907422af942836e
Andy <shumaozhang@yahoo.com>
2014-11-06 22:41:43 +0800

    indeed

--- interview/src/com/andy/indeed/Main2.java
+++ interview/src/com/andy/indeed/Main2.java
@@ -11,18 +11,18 @@ public class Main2 {
 		String ss2 = scanner.next();
 		String ss3 = scanner.next();
 
-		int j=0;
-		int h=0;
-		for (int i = 0; i < ss3.length(); i++) {
-
+		int j = 0;
+		int h = 0;
+		int len = ss3.length();
+		for (int i = 0; i < len; i++) {
 			char temp = ss3.charAt(i);
-			if (ss1.contains(temp + "")&&j<(ss3.length()/2)) {
+			if (ss1.contains(temp + "") && j < (len / 2)) {
 				ss1 = ss1.substring(0, i) + ss1.substring(i + 1);
 				ss3 = ss3.substring(0, i) + ss3.substring(i + 1);
 				i--;
 				j++;
-				
-			} else if (ss2.contains(temp + "")&&h<(ss3.length()/2)) {
+
+			} else if (ss2.contains(temp + "") && h < (len / 2)) {
 				ss2 = ss2.substring(0, i) + ss2.substring(i + 1);
 				ss3 = ss3.substring(0, i) + ss3.substring(i + 1);
 				i--;
@@ -30,7 +30,7 @@ public class Main2 {
 			}
 
 		}
-		if (ss3.length()==0) {
+		if (ss3.length() == 0) {
 			System.out.println("YES");
 		} else {
 			System.out.println("NO");
